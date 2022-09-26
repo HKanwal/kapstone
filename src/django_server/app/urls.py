bf4dc845db6618874a2f03787b540c266410b978
Harkeerat Kanwal <hsk532991@gmail.com>
2022-09-25 21:41:47 -0400

    Clear the slate

--- src/django_server/app/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index),
-    path('example-page', views.example_page),
-]
--- src/next-app/pages/example-page.tsx
+++
@@ -1,8 +0,0 @@
-import { NextPage } from 'next';
-import styles from '../styles/ExamplePage.module.css';
-
-const ExamplePage: NextPage = () => {
-  return <span className={styles.title}>Hello World from the Example Page!</span>;
-};
-
-export default ExamplePage;
