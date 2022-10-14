0d87abd742fb172ee78361492f5d616a135724d5
Harkeerat Kanwal <hsk532991@gmail.com>
2022-10-14 08:01:39 -0400

    Create component for form field

--- src/next-app/components/Button.tsx
+++ src/next-app/components/Button.tsx
@@ -3,11 +3,16 @@ import styles from "../styles/Button.module.css";
 type ButtonProps = {
   title: string;
   width?: string | number;
+  onClick?: () => void;
 };
 
 const Button = (props: ButtonProps) => {
   return (
-    <button className={styles.button} style={{ width: props.width }}>
+    <button
+      className={styles.button}
+      style={{ width: props.width }}
+      onClick={() => props.onClick && props.onClick()}
+    >
       {props.title}
     </button>
   );
