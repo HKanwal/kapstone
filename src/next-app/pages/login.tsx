c210f4f238c7933fa88d0039f4ffb7f6815066c5
HKanwal <hsk532991@gmail.com>
2022-10-20 16:16:32 -0400

    Created janky reset password modal.

--- src/next-app/pages/login.tsx
+++ src/next-app/pages/login.tsx
@@ -1,9 +1,17 @@
 import type { NextPage } from "next";
+import { useState } from "react";
 import Button from "../components/Button";
 import FormField from "../components/FormField";
+import Modal from "../components/Modal";
 import styles from "../styles/LoginPage.module.css";
 
 const LoginPage: NextPage = () => {
+  const [modalVisible, setModalVisisble] = useState<boolean>(false);
+
+  const handleClickForgotPwd = () => {
+    setModalVisisble(true);
+  };
+
   return (
     <div className={styles.container}>
       <div className={styles["username-container"]}>
@@ -12,8 +20,19 @@ const LoginPage: NextPage = () => {
       <div className={styles["password-container"]}>
         <FormField name="Password" placeholder="Enter your password" />
       </div>
-      <span className={styles["forgot-password"]}>Forgot Password?</span>
+      <div className={styles["forgot-password-container"]}>
+        <span className={styles["forgot-password"]} onClick={handleClickForgotPwd}>
+          Forgot Password?
+        </span>
+      </div>
       <Button title="Login" width="80%" />
+
+      <Modal visible={modalVisible}>
+        <span className={styles["reset-password-title"]}>
+          Reset Password
+        </span>
+        <FormField name="Email" placeholder="Enter your email" />
+      </Modal>
     </div>
   );
 };
