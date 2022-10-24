b6a5b248689cb8bdb9ad40f75c05839f9a6c8b9d
HKanwal <hsk532991@gmail.com>
2022-10-24 17:25:02 -0400

    Started 'Create Shop' page

---
+++ src/next-app/pages/create-shop.tsx
@@ -0,0 +1,29 @@
+import type { NextPage } from "next";
+import FormField from "../components/FormField";
+import Header from "../components/Header";
+import styles from "../styles/CreateShopPage.module.css";
+
+const CreateShopPage: NextPage = () => {
+  return (
+    <div className={styles.container}>
+      <Header goBackToHref="/login" title="Create New Shop" />
+
+      <div className={styles.content}>
+        <div className={styles['shop-name-container']}>
+          <FormField name="Shop Name" placeholder="Enter your shop's name" required />
+        </div>
+        <div className={styles['shop-address-container']}>
+          <FormField name="Shop Address" placeholder="Enter your shop's address" required />
+        </div>
+        <div className={styles['shop-phones-container']}>
+          <FormField name="Shop Phone Number(s)" placeholder="Enter your shop's phone number" />
+        </div>
+        <div className={styles['shop-email-container']}>
+          <FormField name="Shop Email" placeholder="Enter your shop's email" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CreateShopPage;
