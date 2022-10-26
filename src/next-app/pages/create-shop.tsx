96d7ab343ea816bfdb42fc0ba5dfc4123dc2ec70
HKanwal <hsk532991@gmail.com>
2022-10-26 14:47:32 -0400

    More fields in shop creation added

--- src/next-app/pages/create-shop.tsx
+++ src/next-app/pages/create-shop.tsx
@@ -1,26 +1,66 @@
 import type { NextPage } from "next";
+import { useState } from "react";
+import Button from "../components/Button";
 import FormField from "../components/FormField";
 import Header from "../components/Header";
 import styles from "../styles/CreateShopPage.module.css";
 
 const CreateShopPage: NextPage = () => {
+  const [name, setName] = useState("");
+  const [address, setAddress] = useState("");
+  const [phoneNumbers, setPhoneNumbers] = useState<string[]>([""]);
+  const [email, setEmail] = useState("");
+  const [employees, setEmployees] = useState("");
+  const [serviceBays, setServiceBays] = useState("");
+
   return (
     <div className={styles.container}>
       <Header goBackToHref="/login" title="Create New Shop" />
 
       <div className={styles.content}>
-        <div className={styles['shop-name-container']}>
-          <FormField name="Shop Name" placeholder="Enter your shop's name" required />
+        <div className={styles['field-container']}>
+          <FormField
+            name="Shop Name"
+            placeholder="Enter your shop's name"
+            onChange={(newVals) => setName(newVals[0])}
+            required />
         </div>
-        <div className={styles['shop-address-container']}>
-          <FormField name="Shop Address" placeholder="Enter your shop's address" required />
+        <div className={styles['field-container']}>
+          <FormField
+            name="Shop Address"
+            placeholder="Enter your shop's address"
+            onChange={(newVals) => setAddress(newVals[0])}
+            required />
         </div>
-        <div className={styles['shop-phones-container']}>
-          <FormField name="Shop Phone Number(s)" placeholder="Enter your shop's phone number" multi="Add phone number" />
+        <div className={styles['field-container']}>
+          <FormField
+            name="Shop Phone Number(s)"
+            placeholder="Enter your shop's phone number"
+            onChange={setPhoneNumbers}
+            multi="Add phone number" />
         </div>
-        <div className={styles['shop-email-container']}>
-          <FormField name="Shop Email" placeholder="Enter your shop's email" />
+        <div className={styles['field-container']}>
+          <FormField
+            name="Shop Email"
+            placeholder="Enter your shop's email"
+            onChange={(newVals) => setEmail(newVals[0])} />
         </div>
+        <div className={styles['field-container']}>
+          <FormField
+            name="Number of Employees"
+            placeholder="Enter number of employees"
+            onChange={(newVals) => setEmployees(newVals[0])} />
+        </div>
+        <div className={styles['field-container']}>
+          <FormField
+            name="Number of Service Bays"
+            placeholder="Enter number of service bays"
+            onChange={(newVals) => setServiceBays(newVals[0])} />
+        </div>
+        <Button
+          title="Create"
+          disabled={name.length < 1 || address.length < 1}
+          width="80%" />
       </div>
     </div>
   );
