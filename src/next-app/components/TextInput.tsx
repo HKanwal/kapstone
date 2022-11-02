8071d461fda92a460164a2f9cc268f5e59b77ca5
Harkeerat Kanwal <hsk532991@gmail.com>
2022-11-02 00:47:14 -0400

    TextInput now gives ref and has handlers for focus and blur

--- src/next-app/components/TextInput.tsx
+++ src/next-app/components/TextInput.tsx
@@ -1,6 +1,16 @@
-import { ChangeEvent, CSSProperties, useEffect, useMemo, useState } from "react";
+import {
+  ChangeEvent,
+  CSSProperties,
+  forwardRef,
+  Ref,
+  useEffect,
+  useImperativeHandle,
+  useMemo,
+  useRef,
+  useState,
+} from "react";
 import styles from "../styles/components/TextInput.module.css";
-import { ImCancelCircle } from 'react-icons/im';
+import { ImCancelCircle } from "react-icons/im";
 
 type TextInputProps = {
   placeholder?: string;
@@ -9,6 +19,8 @@ type TextInputProps = {
   onChange?: (newVal: string) => void;
   type?: string;
   onRemove?: () => void;
+  onFocus?: () => void;
+  onBlur?: () => void;
   style?: {
     paddingLeft?: string;
     paddingRight?: string;
@@ -17,67 +29,88 @@ type TextInputProps = {
   };
 };
 
+type TextInputRef = {
+  focus: () => void;
+};
+
 const calcSize = (winHeight: number, winWidth: number) => {
   return Math.floor(Math.min(winHeight, winWidth) / 20);
 };
 
-const TextInput = (props: TextInputProps) => {
-  const [value, setValue] = useState(props.value ?? '');
-  const [cancelSize, setCancelSize] = useState(0);
-  const style: CSSProperties = useMemo(() => {
-    return {
-      width: props.width ?? '100%',
-      ...props.style,
-      paddingRight: props.onRemove ? '3em' : (props.style?.paddingRight ?? '1em'),
+const TextInput = forwardRef(
+  (props: TextInputProps, ref: Ref<TextInputRef>) => {
+    const [value, setValue] = useState(props.value ?? "");
+    const [cancelSize, setCancelSize] = useState(0);
+    const inputRef = useRef<HTMLInputElement>(null);
+    const style: CSSProperties = useMemo(() => {
+      return {
+        width: props.width ?? "100%",
+        ...props.style,
+        paddingRight: props.onRemove
+          ? "3em"
+          : props.style?.paddingRight ?? "1em",
+      };
+    }, [props.width, props.style]);
+
+    useImperativeHandle(ref, () => {
+      return {
+        focus: () => {
+          inputRef.current?.focus();
+        },
+      };
+    });
+
+    useEffect(() => {
+      const handleResize = () => {
+        setCancelSize(calcSize(window.innerHeight, window.innerWidth));
+      };
+
+      handleResize();
+      window.addEventListener("resize", handleResize);
+
+      return () => {
+        window.removeEventListener("resize", handleResize);
+      };
+    }, []);
+
+    useEffect(() => {
+      setValue(props.value ?? "");
+    }, [props.value]);
+
+    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+      const newVal = e.target.value;
+      props.onChange && props.onChange(newVal);
+      setValue(newVal);
     };
-  }, [props.width, props.style]);
 
-  useEffect(() => {
-    const handleResize = () => {
-      setCancelSize(calcSize(window.innerHeight, window.innerWidth));
+    const handleCancel = () => {
+      props.onRemove && props.onRemove();
     };
 
-    handleResize();
-    window.addEventListener("resize", handleResize);
-
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, []);
-
-  useEffect(() => {
-    setValue(props.value ?? '');
-  }, [props.value]);
-
-  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-    const newVal = e.target.value;
-    props.onChange && props.onChange(newVal);
-    setValue(newVal);
-  };
-
-  const handleCancel = () => {
-    props.onRemove && props.onRemove();
-  };
-
-  return (
-    <div className={styles.container}>
-      <input
-        className={styles.input}
-        type={props.type ?? "text"}
-        placeholder={props.placeholder ?? ""}
-        value={value}
-        onChange={handleChange}
-        style={style}
-      />
-      {
-        (props.onRemove) ?
-          <div className={styles['cancel-button']} onClick={handleCancel}>
+    return (
+      <div className={styles.container}>
+        <input
+          className={styles.input}
+          type={props.type ?? "text"}
+          placeholder={props.placeholder ?? ""}
+          value={value}
+          onChange={handleChange}
+          style={style}
+          onFocus={props.onFocus}
+          onBlur={props.onBlur}
+          ref={inputRef}
+        />
+        {props.onRemove ? (
+          <div className={styles["cancel-button"]} onClick={handleCancel}>
             <ImCancelCircle size={cancelSize} />
           </div>
-          : <></>
-      }
-    </div>
-  );
-};
+        ) : (
+          <></>
+        )}
+      </div>
+    );
+  }
+);
 
+export type { TextInputRef };
 export default TextInput;
