601b2579413c9199a2fa4faafdf1410c2b0a4e1a
Himanshu Aggarwal <aggarwal.h@outlook.com>
2022-11-05 16:27:45 -0400

    Fix UserCreateSerializer to include type field

--- src/django-app/django-project/accounts/serializers.py
+++ src/django-app/django-project/accounts/serializers.py
@@ -1,25 +1,14 @@
-from djoser.serializers import UserCreateSerializer
+# from typing_extensions import override
+from djoser.serializers import UserCreateSerializer as BaseUserCreateSerializer
 from django.contrib.auth import get_user_model
-from rest_framework import serializers
+# from rest_framework import serializers
 
 User = get_user_model()
 
 
-class UserCreateSerializer(UserCreateSerializer):
-    type = serializers.ChoiceField(choices=User.Types, allow_blank=False)
+class UserCreateSerializer(BaseUserCreateSerializer):
 
-    class Meta(UserCreateSerializer.Meta):
+    class Meta(BaseUserCreateSerializer.Meta):
         model = User
         fields = ("username", "password", "email", "first_name", "last_name", "type")
 
-    def create(self, validated_data):
-        user = User.objects.create(
-            username=validated_data["username"],
-            email=validated_data["email"],
-            first_name=validated_data["first_name"],
-            last_name=validated_data["last_name"],
-            type=validated_data["type"],
-        )
-        user.set_password(validated_data["password"])
-        user.save()
-        return user
