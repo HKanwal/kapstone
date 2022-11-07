b763fd7b0eb692f63e77ef4201e914e9517b0a72
tevisdoe <tevis.doe@gmail.com>
2022-11-07 17:32:33 -0500

    Update path-prefix for shops in url

--- src/django-app/django-project/config/urls.py
+++ src/django-app/django-project/config/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.jwt')),
-    path('', include('shops.urls')),
+    path('shops/', include('shops.urls')),
 ]
