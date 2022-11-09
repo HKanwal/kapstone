583e4753bde3273c1cc2f2051cd7945c41aba073
tevisdoe <tevis.doe@gmail.com>
2022-11-09 16:01:35 -0500

    Update status choices field

--- src/django-app/django-project/quotes/models.py
+++ src/django-app/django-project/quotes/models.py
@@ -2,22 +2,18 @@ from django.db import models
 from django.utils.translation import gettext as _
 from shops.models import Shop
 
+class Status(models.TextChoices):
+    NEW_QUOTE = "new_quote", "New Quote"
+    ACCEPTED = "accepted", "Accepted"
+    IN_PROGRESS = "in_progress", "In Progress"
+    DONE = "done", "Done"
+    REWORK = "rework", "Rework"
+
+
 class Quote(models.Model):
     quote_request = models.OneToOneField("QuoteRequest", on_delete=models.CASCADE)
     shop = models.ForeignKey(Shop, on_delete=models.CASCADE)
-    new_quote = 'NEW_QUOTE'
-    accepted = 'ACCEPTED'
-    in_progress = 'IN_PROGRESS'
-    done = 'DONE'
-    rework = 'REWORK'
-    status_choices = [
-        (new_quote, 'New Quote'),
-        (accepted, 'Accepted'),
-        (in_progress, 'In Progress'),
-        (done, 'Done'),
-        (rework, 'Rework')
-    ]
-    status = models.CharField(_("status"), max_length=12, choices=status_choices, default=new_quote)
+    status = models.CharField(_("status"), max_length=12, choices=Status.choices, default=Status.NEW_QUOTE)
     price = models.DecimalField(_("price"), max_digits=19, decimal_places=2)
     estimated_time= models.CharField(_("estimated time"), max_length=255)
     expiry_date= models.DateField(_("expiry date"))
--- src/django-app/django-project/quotes/serializers.py
+++ src/django-app/django-project/quotes/serializers.py
@@ -1,25 +1,12 @@
 from rest_framework import serializers
 
-from .models import Quote, QuoteRequest
+from .models import Quote, QuoteRequest, Status
 from shops.models import Shop
-
-new_quote = 'NEW_QUOTE'
-accepted = 'ACCEPTED'
-in_progress = 'IN_PROGRESS'
-done = 'DONE'
-rework = 'REWORK'
-status_choices = [
-    (new_quote, 'New Quote'),
-    (accepted, 'Accepted'),
-    (in_progress, 'In Progress'),
-    (done, 'Done'),
-    (rework, 'Rework')
-]
 
 class QuoteSerializer(serializers.ModelSerializer):
     shop = serializers.PrimaryKeyRelatedField(queryset=Shop.objects.all())
     quote_request = serializers.PrimaryKeyRelatedField(queryset=QuoteRequest.objects.all())
-    status = serializers.ChoiceField(choices=status_choices)
+    status = serializers.ChoiceField(choices=Status.choices)
 
     class Meta:
         model = Quote
