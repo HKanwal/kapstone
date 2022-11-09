154123e1e6692c98fdbb990dd2a17b9d48250ad5
Himanshu Aggarwal <aggarwal.h@outlook.com>
2022-11-09 17:15:12 -0500

    Add initial Invitation model

--- src/django-app/django-project/shops/models.py
+++ src/django-app/django-project/shops/models.py
@@ -1,24 +1,36 @@
+import uuid
 from django.db import models
-    
+
 from django.utils.translation import gettext as _
 from django.apps import apps
+
 
 class Shop(models.Model):
     shop_owner = models.ForeignKey("accounts.ShopOwner", on_delete=models.CASCADE)
     address = models.OneToOneField("Address", on_delete=models.PROTECT)
     name = models.CharField(_("shop name"), max_length=255, unique=True)
-    num_bays = models.IntegerField(_("number of service bays"), default=1, null=True, blank=True)
-    availability = models.CharField(_("available hours"), max_length=255, null=True, blank=True)
-    shop_hours = models.CharField(_("shop hours"), max_length=255, null=True, blank=True)
-    
+    num_bays = models.IntegerField(
+        _("number of service bays"), default=1, null=True, blank=True
+    )
+    availability = models.CharField(
+        _("available hours"), max_length=255, null=True, blank=True
+    )
+    shop_hours = models.CharField(
+        _("shop hours"), max_length=255, null=True, blank=True
+    )
+
     @property
     def num_employees(self):
-        EmployeeData = apps.get_model('accounts', 'EmployeeData')
+        EmployeeData = apps.get_model("accounts", "EmployeeData")
         return EmployeeData.objects.filter(shop__pk=self.pk).count()
-    
+
     class Meta:
         verbose_name = "Shop"
         verbose_name_plural = "Shops"
+
+    def __str__(self):
+        return f"{self.name}"
+
 
 class Address(models.Model):
     postal_code = models.CharField(_("postal code"), max_length=6)
@@ -32,5 +44,22 @@ class Address(models.Model):
         verbose_name_plural = "Addresses"
 
     def __str__(self):
-        return self.street + ", " + self.city + ", " + self.province + ", " + self.country + ", " + self.postal_code
+        return f"{self.street}, {self.city}, {self.province}, {self.country}, {self.postal_code}"
 
+
+class Invitation(models.Model):
+    shop = models.ForeignKey(Shop, on_delete=models.CASCADE)
+    email = models.EmailField(_("email address"), max_length=255, unique=False)
+    invitation_key = models.UUIDField(
+        _("UUID"), primary_key=True, default=uuid.uuid4, editable=False
+    )
+    is_expired = models.BooleanField(default=False)
+    is_used = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = "Invitation"
+        verbose_name_plural = "Invitations"
+
+    def __str__(self):
+        return f'Invitation for {self.email} from "{self.shop.name}".'
