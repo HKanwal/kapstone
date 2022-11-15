81085cb151d6bd3c9c4426a38b9afcf019f94901
Himanshu Aggarwal <aggarwal.h@outlook.com>
2022-11-15 00:41:13 -0500

    Update static root config

--- src/django-app/config/settings.py
+++ src/django-app/config/settings.py
@@ -179,9 +179,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = "/static/"
+STATIC_URL = "/assets/"
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
-STATIC_ROOT = os.path.join(BASE_DIR, "assests")
+STATIC_ROOT = os.path.join(BASE_DIR, "assets")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
