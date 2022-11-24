5d6db627665c0f58b3eabb8c9178ba75241be4e6
Harkeerat Kanwal <hsk532991@gmail.com>
2022-11-24 18:37:45 -0500

    Fix api-url

--- src/next-app/constants/api-url.ts
+++ src/next-app/constants/api-url.ts
@@ -1,2 +1,2 @@
-export default 'http://localhost:8000';
-// export default 'https://walrus-app-5juci.ondigitalocean.app';
+// export default 'http://localhost:8000';
+export default 'https://walrus-app-5juci.ondigitalocean.app';
