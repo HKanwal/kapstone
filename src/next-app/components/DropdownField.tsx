7b33a60f73045f4faec2800588da9857812685f7
Harkeerat Kanwal <hsk532991@gmail.com>
2022-11-25 00:30:01 -0500

    Rethink DropdownField logic and fix bugs

--- src/next-app/components/DropdownField.tsx
+++ src/next-app/components/DropdownField.tsx
@@ -1,4 +1,4 @@
-import { useMemo, useRef, useState } from 'react';
+import { useEffect, useMemo, useRef, useState } from 'react';
 import styles from '../styles/components/DropdownField.module.css';
 import TextInput, { TextInputRef } from './TextInput';
 import { BsChevronDown } from 'react-icons/bs';
@@ -11,6 +11,7 @@ type DropdownFieldProps = {
   width?: string | number;
   required?: boolean;
   items: string[];
+  selectedItems?: string[];
   onSelect?: (item: string, selectedItems: string[]) => void;
   type?: Type;
 };
@@ -18,37 +19,49 @@ type DropdownFieldProps = {
 let collapseTimout: NodeJS.Timeout;
 
 const DropdownField = (props: DropdownFieldProps) => {
-  const [selectedItems, setSelectedItems] = useState<string[]>([]);
+  const [selectedItems, setSelectedItems] = useState<string[]>(props.selectedItems ?? []);
   const [expanded, setExpanded] = useState(false);
   const [value, setValue] = useState('');
   const items = useMemo(() => Array.from(new Set(props.items)), [props.items]);
   const inputRef = useRef<TextInputRef>(null);
   const type: Type = props.type === 'multi-select' ? 'multi-select' : 'single-select';
 
+  useEffect(() => {
+    setSelectedItems(props.selectedItems ?? []);
+  }, [props.selectedItems]);
+
   const handleSelect = (item: string) => {
     props.onSelect && props.onSelect(item, selectedItems);
   };
 
-  const handleBlur = () => {
+  const handleInputBlur = () => {
     collapseTimout = setTimeout(() => {
       setExpanded(false);
+      if (type === 'single-select' && selectedItems[0] !== undefined) {
+        setValue(selectedItems[0]);
+      } else {
+        setValue('');
+      }
     }, 10);
   };
 
-  const handleFocus = () => {
-    clearTimeout(collapseTimout);
+  const handleInputFocus = () => {
     setValue('');
     setExpanded(true);
-    inputRef.current?.focus();
+  };
+
+  const handleBtnFocus = () => {
+    clearTimeout(collapseTimout);
   };
 
   const handleItemClick = (item: string) => {
     setSelectedItems((prevSelectedItems) => {
       if (type === 'single-select') {
         setValue(item);
-        inputRef.current?.blur();
+        setExpanded(false);
         return [item];
       } else {
+        inputRef.current?.focus();
         return [...prevSelectedItems, item];
       }
     });
@@ -58,11 +71,11 @@ const DropdownField = (props: DropdownFieldProps) => {
   const handleChipMouseDown = () => {
     setTimeout(() => {
       clearTimeout(collapseTimout);
-      inputRef.current?.focus();
     }, 10);
   };
 
   const handleRemove = (item: string) => {
+    inputRef.current?.focus();
     setSelectedItems((prevSelectedItems) => {
       return prevSelectedItems.filter((si) => {
         return si !== item;
@@ -100,8 +113,8 @@ const DropdownField = (props: DropdownFieldProps) => {
           style={{ paddingRight: '14%' }}
           value={value}
           onChange={setValue}
-          onFocus={handleFocus}
-          onBlur={handleBlur}
+          onFocus={handleInputFocus}
+          onBlur={handleInputBlur}
           ref={inputRef}
         />
         <div className={styles['chevron-container']}>
@@ -111,7 +124,7 @@ const DropdownField = (props: DropdownFieldProps) => {
           <div className={styles['dropdown']}>
             {items
               .filter((item) => {
-                return !selectedItems.includes(item);
+                return type === 'multi-select' ? !selectedItems.includes(item) : true;
               })
               .filter((item) => {
                 return value.length === 0 || item.startsWith(value);
@@ -120,8 +133,7 @@ const DropdownField = (props: DropdownFieldProps) => {
                 <button
                   className={styles.item}
                   key={item}
-                  onFocus={handleFocus}
-                  onBlur={handleBlur}
+                  onFocus={handleBtnFocus}
                   onClick={() => handleItemClick(item)}>
                   {item}
                 </button>
