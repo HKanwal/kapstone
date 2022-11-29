c46ac7fef3d0814cdc0d263bdfd4db128445ccb7
Himanshu Aggarwal <aggarwal.h@outlook.com>
2022-11-29 16:58:22 -0500

    Add Appointment/Service/WorkOrder models/admin config

--- src/django-app/shops/admin.py
+++ src/django-app/shops/admin.py
@@ -2,12 +2,39 @@ from django.contrib import admin
 from .models import *
 
 
+@admin.register(Shop)
+class ShopAdmin(admin.ModelAdmin):
+    list_display = ("name", "shop_owner")
+
+
+@admin.register(Invitation)
 class InvitationAdmin(admin.ModelAdmin):
     list_display = ("shop", "email", "invitation_key")
     fields = ("shop", "email", "invitation_key", "is_used", "is_expired")
     readonly_fields = ["invitation_key"]
 
 
-admin.site.register(Shop)
-admin.site.register(Address)
-admin.site.register(Invitation, InvitationAdmin)
+@admin.register(Address)
+class AddressAdmin(admin.ModelAdmin):
+    list_display = ("street", "city", "province", "country", "postal_code")
+
+
+@admin.register(Service)
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = ("name", "shop")
+
+
+@admin.register(ServicePart)
+class ServicePartAdmin(admin.ModelAdmin):
+    list_display = ("service", "part", "quantity")
+
+
+@admin.register(Appointment)
+class AppointmentAdmin(admin.ModelAdmin):
+    list_display = ("shop", "customer", "date", "time", "duration")
+
+@admin.register(WorkOrder)
+class WorkOrderAdmin(admin.ModelAdmin):
+    list_display = ("shop", "appointment", "employee", "grand_total")
+
+
--- src/django-app/shops/models.py
+++ src/django-app/shops/models.py
@@ -7,6 +7,9 @@ from django.conf import settings
 from django.core.mail import EmailMultiAlternatives
 from django.template.loader import render_to_string
 
+from vehicles.models import Vehicle, Part
+from .validators import validate_nonzero
+
 
 class Shop(models.Model):
     shop_owner = models.ForeignKey("accounts.ShopOwner", on_delete=models.CASCADE)
@@ -61,7 +64,7 @@ class Invitation(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
-        unique_together = ('shop', 'email')
+        unique_together = ("shop", "email")
         verbose_name = "Invitation"
         verbose_name_plural = "Invitations"
 
@@ -90,3 +93,99 @@ class Invitation(models.Model):
 
     def __str__(self):
         return f'Invitation for {self.email} from "{self.shop.name}".'
+
+
+class Appointment(models.Model):
+    # class Status(models.TextChoices):
+    #     COMPLETED = "completed", "Completed"
+    #     NO_SHOW = "no show", "No Show"
+    #     IN_PROGRESS = "in_progress", "In Progress"
+    #     DONE = "done", "Done"
+    #     REWORK = "rework", "Rework"
+    # status = models.CharField(
+    #     _("status"), max_length=12, choices=Status.choices, default=Status.NEW_QUOTE
+    # )
+    shop = models.ForeignKey(Shop, on_delete=models.CASCADE)
+    customer = models.ForeignKey(
+        "accounts.Customer", on_delete=models.CASCADE, null=True
+    )
+    # TODO: add customer details when customer doesn't book appointment/shows up to the store
+    # customer_email = models.EmailField(_("email address"), max_length=255, null=True)
+    # customer_first_name = models.CharField(_("first name"), max_length=255, blank=True)
+    # customer_last_name = models.CharField(_("last name"), max_length=255, blank=True)
+    # customer_phone_number = PhoneNumberField(blank=True)
+    vehicle = models.ForeignKey(Vehicle, on_delete=models.CASCADE)
+    date = models.DateField(_("date"), blank=True)
+    time = models.TimeField(_("time"), blank=True)
+    duration = models.PositiveIntegerField(_("appointment duration in hours"), null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = "Appointment"
+        verbose_name_plural = "Appointments"
+        # constraints = [
+        #     models.CheckConstraint(
+        #         name="%(app_label)s_%(class)s_customer_details_or_customer_id",
+        #         check=(
+        #             models.Q(customer__isnull=True, customer_first_name__exact='', customer_first_name__exact='', customer_first_name__exact='')
+        #             | models.Q(thing1__isnull=False, thing2__isnull=True)
+        #         ),
+        #     )
+        # ]
+
+    def __str__(self):
+        return f"{self.shop} - {self.customer} - {self.date} - {self.time}"
+
+
+class Service(models.Model):
+    shop = models.ForeignKey(Shop, on_delete=models.CASCADE)
+    name = models.CharField(_("service name"), max_length=30, null=False, blank=False)
+    description = models.TextField(_("service description"), null=True, blank=True)
+    price = models.DecimalField(
+        _("service price"), max_digits=10, decimal_places=2, null=False
+    )
+    parts = models.ManyToManyField(Part, through="ServicePart")
+
+    class Meta:
+        unique_together = ("shop", "name")
+        verbose_name = "Service"
+        verbose_name_plural = "Services"
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+class ServicePart(models.Model):
+    service = models.ForeignKey(Service, on_delete=models.CASCADE)
+    part = models.ForeignKey(Part, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(
+        _("number of parts"), default=1, null=False, validators=[validate_nonzero]
+    )
+
+    class Meta:
+        verbose_name = "ServicePart"
+        verbose_name_plural = "ServiceParts"
+
+
+class WorkOrder(models.Model):
+    appointment = models.OneToOneField(Appointment, on_delete=models.CASCADE)
+    quote = models.ForeignKey("quotes.Quote", on_delete=models.CASCADE, null=False)
+    shop = models.ForeignKey(Shop, on_delete=models.CASCADE)
+    employee = models.ForeignKey("accounts.Employee", on_delete=models.CASCADE)
+    odometer_reading_before = models.PositiveIntegerField(
+        _("odometer reading before"), null=True
+    )
+    odometer_reading_after = models.PositiveIntegerField(
+        _("odometer reading after"), null=True
+    )
+    discount = models.DecimalField(
+        _("discount percentage"), max_digits=5, decimal_places=2, default=0
+    )
+    notes = models.TextField(_("notes"), blank=True)
+    grand_total = models.DecimalField(
+        _("grand total amount"), max_digits=10, decimal_places=2
+    )
+
+    class Meta:
+        verbose_name = "Work Order"
+        verbose_name_plural = "Work Orders"
