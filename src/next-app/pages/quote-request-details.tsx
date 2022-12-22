7f6d87118cdc42cc2cc0045073b2d91ad6108f2e
Rachel Johnson <rachelj1098@yahoo.ca>
2022-12-22 18:13:23 -0500

    fixed errors

--- src/next-app/pages/quote-request-details.tsx
+++ src/next-app/pages/quote-request-details.tsx
@@ -1,20 +1,27 @@
 /* eslint-disable indent */
 import type { NextPage } from 'next';
-import { useState } from 'react';
+import { ChangeEvent, useEffect, useRef, useState } from 'react';
+import Button from '../components/Button';
+import TextField from '../components/TextField';
 import Header from '../components/Header';
 import styles from '../styles/pages/QuoteRequestDetails.module.css';
+import TextArea from '../components/TextArea';
+import DropdownField from '../components/DropdownField';
 import Dropdown from '../components/Dropdown';
 import FieldLabel from '../components/FieldLabel';
 import TextInput from '../components/TextInput';
+import ShopCard from '../components/ShopCard';
 import Card from '../components/Card';
+import apiUrl from '../constants/api-url';
 import { quotes } from '../data/QuoteData';
 
 const QuoteRequestDetailsPage: NextPage = () => {
   const [status, setStatus] = useState('All');
   const [sortItem, setSortItem] = useState('');
-  /**const [sortMethod, setSortMethod] = useState('');**/
-  function noChange(): void {}
-
+  const [sortMethod, setSortMethod] = useState('');
+  function noChange(): void {
+    throw new Error('Function not implemented.');
+  }
   return (
     <div className={styles.container}>
       <Header title="Quote Request - " />
@@ -46,7 +53,6 @@ const QuoteRequestDetailsPage: NextPage = () => {
             {status != 'All' && status != 'Accepted'
               ? quotes
                   .filter((quote) => quote.status == status)
-                  .sort((a, b) => (Date.parse(a.date) > Date.parse(b.date) ? 1 : -1))
                   .map((item, index) => {
                     return (
                       <Card
@@ -61,7 +67,7 @@ const QuoteRequestDetailsPage: NextPage = () => {
               : status == 'Accepted' && sortItem == 'Date'
               ? quotes
                   .filter((quote) => quote.status == status)
-                  .sort((a, b) => (Date.parse(a.date) > Date.parse(b.date) ? 1 : -1))
+                  .sort((a, b) => (a.date > b.date ? -1 : 1))
                   .map((item, index) => {
                     return (
                       <Card
@@ -88,19 +94,17 @@ const QuoteRequestDetailsPage: NextPage = () => {
                       />
                     );
                   })
-              : quotes
-                  .sort((a, b) => (Date.parse(a.date) > Date.parse(b.date) ? 1 : -1))
-                  .map((item, index) => {
-                    return (
-                      <Card
-                        key={index}
-                        name={item['shop-name']}
-                        status={item.status}
-                        date={item.date}
-                        price={item.price}
-                      />
-                    );
-                  })}
+              : quotes.map((item, index) => {
+                  return (
+                    <Card
+                      key={index}
+                      name={item['shop-name']}
+                      status={item.status}
+                      date={item.date}
+                      price={item.price}
+                    />
+                  );
+                })}
           </div>
         </div>
 
@@ -153,7 +157,6 @@ const QuoteRequestDetailsPage: NextPage = () => {
 /**export async function getQuotes() {
   const res = await fetch(`${apiUrl}/quotes/quotes/`)
   const data = await res.json()
-
   return{
     props: {
       quotes : data
