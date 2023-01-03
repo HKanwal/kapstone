0513bfef3b5ed611089f0991e93d0d60d82f7683
Himanshu Aggarwal <aggarwal.h@outlook.com>
2023-01-02 23:15:27 -0500

    Update quote serializers

--- src/django-app/quotes/serializers.py
+++ src/django-app/quotes/serializers.py
@@ -2,21 +2,35 @@ from rest_framework import serializers
 
 from .models import Quote, QuoteRequest
 from shops.models import Shop
+from shops.serializers import ShopOverviewSerializer
 from accounts.models import Customer
+from accounts.serializers import UserViewSerializer
+
 
 class QuoteSerializer(serializers.ModelSerializer):
     shop = serializers.PrimaryKeyRelatedField(queryset=Shop.objects.all())
-    quote_request = serializers.PrimaryKeyRelatedField(queryset=QuoteRequest.objects.all())
+    quote_request = serializers.PrimaryKeyRelatedField(
+        queryset=QuoteRequest.objects.all()
+    )
     status = serializers.ChoiceField(choices=Quote.Status.choices)
 
     class Meta:
         model = Quote
-        fields = '__all__'
+        fields = "__all__"
+
 
 class QuoteRequestSerializer(serializers.ModelSerializer):
-    shop = serializers.PrimaryKeyRelatedField(many=True, queryset=Shop.objects.all())
-    customer = serializers.PrimaryKeyRelatedField(queryset=Customer.objects.all())
-    
+    shop = ShopOverviewSerializer()
+    customer = UserViewSerializer(source="user")
+
     class Meta:
         model = QuoteRequest
-        fields = '__all__'
+        fields = (
+            "id",
+            "shop",
+            "customer",
+            "preferred_date",
+            "preferred_time",
+            "description",
+        )
+        read_only_fields = ["customer"]
