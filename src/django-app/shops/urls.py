00ddcda2884424098f341664eb5a57ddaa494cc6
Himanshu Aggarwal <aggarwal.h@outlook.com>
2023-01-02 23:14:08 -0500

    Add service urls

--- src/django-app/shops/urls.py
+++ src/django-app/shops/urls.py
@@ -6,6 +6,7 @@ router = routers.DefaultRouter()
 router.register(r'shops', views.ShopViewSet)
 router.register(r'addresses', views.AddressViewSet)
 router.register(r'invitations', views.InvitationViewSet)
+router.register(r'services', views.ServiceViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
