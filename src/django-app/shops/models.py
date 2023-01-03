a77dffae6e5035aaf5def436ffafc9e1486b6ae8
Himanshu Aggarwal <aggarwal.h@outlook.com>
2023-01-02 23:46:59 -0500

    Add appointment and availability models

--- src/django-app/shops/models.py
+++ src/django-app/shops/models.py
@@ -1,11 +1,19 @@
 import uuid
-from django.db import models
+from django.db import models, transaction
 
 from django.utils.translation import gettext as _
 from django.apps import apps
 from django.conf import settings
 from django.core.mail import EmailMultiAlternatives
 from django.template.loader import render_to_string
+from django.contrib.postgres.fields import ArrayField
+from django.db.models import CheckConstraint, UniqueConstraint, Q, F
+from datetime import timedelta
+from django.db.models.lookups import IntegerLessThan
+from django.core.exceptions import ValidationError
+
+
+from phonenumber_field.modelfields import PhoneNumberField
 
 from vehicles.models import Vehicle, Part
 from .validators import validate_nonzero
@@ -13,6 +21,8 @@ from .validators import validate_nonzero
 
 class Shop(models.Model):
     shop_owner = models.ForeignKey("accounts.ShopOwner", on_delete=models.CASCADE)
+    # shop_email = models.EmailField(_("shop's email address"), max_length=255, null=True)
+    # shop_phone_numbers = ArrayField(PhoneNumberField(blank=True), blank=True)
     address = models.OneToOneField("Address", on_delete=models.PROTECT)
     name = models.CharField(_("shop name"), max_length=255, unique=True)
     num_bays = models.IntegerField(
@@ -96,55 +106,148 @@ class Invitation(models.Model):
 
 
 class Appointment(models.Model):
-    # class Status(models.TextChoices):
-    #     COMPLETED = "completed", "Completed"
-    #     NO_SHOW = "no show", "No Show"
-    #     IN_PROGRESS = "in_progress", "In Progress"
-    #     DONE = "done", "Done"
-    #     REWORK = "rework", "Rework"
-    # status = models.CharField(
-    #     _("status"), max_length=12, choices=Status.choices, default=Status.NEW_QUOTE
-    # )
+    class Status(models.TextChoices):
+        COMPLETED = "completed", "Completed"
+        NO_SHOW = "no show", "No Show"
+        IN_PROGRESS = "in_progress", "In Progress"
+        DONE = "done", "Done"
+        REWORK = "rework", "Rework"
+        PENDING = "pending", "Pending"
+
+    status = models.CharField(
+        _("status"), max_length=12, choices=Status.choices, default=Status.PENDING
+    )
     shop = models.ForeignKey(Shop, on_delete=models.CASCADE)
     customer = models.ForeignKey(
         "accounts.Customer", on_delete=models.CASCADE, null=True
     )
-    # TODO: add customer details when customer doesn't book appointment/shows up to the store
-    # customer_email = models.EmailField(_("email address"), max_length=255, null=True)
-    # customer_first_name = models.CharField(_("first name"), max_length=255, blank=True)
-    # customer_last_name = models.CharField(_("last name"), max_length=255, blank=True)
-    # customer_phone_number = PhoneNumberField(blank=True)
     vehicle = models.ForeignKey(Vehicle, on_delete=models.CASCADE)
-    date = models.DateField(_("date"), blank=True)
-    time = models.TimeField(_("time"), blank=True)
-    duration = models.PositiveIntegerField(_("appointment duration in hours"), null=True, blank=True)
+    duration = models.DurationField(_("Duration"), default=timedelta(minutes=30))
     created_at = models.DateTimeField(auto_now_add=True)
+
+    @property
+    def start_time(self):
+        first_slot = (
+            Appointment.objects.get(id=self.id)
+            .appointmentslot_set.order_by("start_time")
+            .first()
+        )
+        if first_slot is not None:
+            return first_slot.start_time
+        return None
+
+    @property
+    def end_time(self):
+        last_slot = (
+            Appointment.objects.get(id=self.id)
+            .appointmentslot_set.order_by("end_time")
+            .last()
+        )
+        if last_slot is not None:
+            return last_slot.end_time
+        return None
 
     class Meta:
         verbose_name = "Appointment"
         verbose_name_plural = "Appointments"
-        # constraints = [
-        #     models.CheckConstraint(
-        #         name="%(app_label)s_%(class)s_customer_details_or_customer_id",
-        #         check=(
-        #             models.Q(customer__isnull=True, customer_first_name__exact='', customer_first_name__exact='', customer_first_name__exact='')
-        #             | models.Q(thing1__isnull=False, thing2__isnull=True)
-        #         ),
-        #     )
-        # ]
 
     def __str__(self):
-        return f"{self.shop} - {self.customer} - {self.date} - {self.time}"
+        return f"{self.shop} - {self.customer} - {self.start_time} - {self.end_time}"
+
+
+class AppointmentSlot(models.Model):
+    shop = models.ForeignKey(Shop, on_delete=models.CASCADE)
+    appointment = models.ManyToManyField(Appointment, blank=True)
+    start_time = models.DateTimeField(null=False)
+    end_time = models.DateTimeField(null=False)
+    num_slots = models.PositiveIntegerField()
+
+    @property
+    def duration(self):
+        return self.end_time - self.start_time
+
+    @property
+    def booked_slots(self):
+        return self.appointment.count()
+
+    @property
+    def is_available(self):
+        return self.num_slots > self.booked_slots
+
+    @property
+    def num_available_slots(self):
+        return self.num_slots - self.booked_slots
+
+    class Meta:
+        verbose_name = "Appointment Slot"
+        verbose_name_plural = "Appointment Slots"
+        constraints = [
+            CheckConstraint(
+                check=Q(end_time__gt=F("start_time")),
+                name="check_start_and_end_time",
+            ),
+            UniqueConstraint(
+                name="unique_appointment_slot",
+                fields=["shop", "start_time", "end_time"],
+            ),
+        ]
+
+    def __str__(self):
+        return f"Slot from {self.start_time} to {self.end_time} - {self.booked_slots}"
+
+
+class ShopAvailability(models.Model):
+    shop = models.ForeignKey(Shop, on_delete=models.CASCADE)
+    date = models.DateField(null=False)
+
+    class Meta:
+        verbose_name = "Shop Availability"
+        verbose_name_plural = "Shop Availability"
+        constraints = [
+            UniqueConstraint(
+                fields=["shop", "date"], name="shop_date_unique_combination"
+            )
+        ]
+
+    def __str__(self):
+        return f"{self.shop} - {self.date}"
+
+
+class ShopAvailabilitySlot(models.Model):
+    """
+    This will store the time slots that a shop is available on a given day.
+    For example: 9am - 3pm, 4pm - 6pm.
+    """
+
+    start_time = models.TimeField(null=False)
+    end_time = models.TimeField(null=False)
+    shop_availability = models.ForeignKey(ShopAvailability, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = "Availability Slot"
+        verbose_name_plural = "Availability Slots"
+        constraints = [
+            CheckConstraint(
+                check=Q(end_time__gt=F("start_time")),
+                name="ensure_start_is_before_end",
+            )
+        ]
+
+    def __str__(self):
+        return f"Available slot: {self.start_time} - {self.end_time}"
 
 
 class Service(models.Model):
-    shop = models.ForeignKey(Shop, on_delete=models.CASCADE)
+    shop = models.ForeignKey(
+        Shop, on_delete=models.CASCADE, related_name="shop_services"
+    )
     name = models.CharField(_("service name"), max_length=30, null=False, blank=False)
     description = models.TextField(_("service description"), null=True, blank=True)
     price = models.DecimalField(
         _("service price"), max_digits=10, decimal_places=2, null=False
     )
     parts = models.ManyToManyField(Part, through="ServicePart")
+    active = models.BooleanField(default=True)
 
     class Meta:
         unique_together = ("shop", "name")
