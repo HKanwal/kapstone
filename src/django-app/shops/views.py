ef323b258a2504a6ee56b32e2ad6c858b214bfce
Himanshu Aggarwal <aggarwal.h@outlook.com>
2023-01-02 23:13:52 -0500

    Add service view set

--- src/django-app/shops/views.py
+++ src/django-app/shops/views.py
@@ -1,18 +1,30 @@
 from django.shortcuts import render
 from rest_framework import viewsets
 
-from .serializers import ShopSerializer, AddressSerializer, InvitationSerializer
-from .models import Shop, Address, Invitation
+from .serializers import (
+    ShopSerializer,
+    AddressSerializer,
+    InvitationSerializer,
+    ServiceSerializer,
+)
+from .models import Shop, Address, Invitation, Service
 
 
 class ShopViewSet(viewsets.ModelViewSet):
-    queryset = Shop.objects.all().order_by('name')
+    queryset = Shop.objects.all().order_by("name")
     serializer_class = ShopSerializer
 
+
 class AddressViewSet(viewsets.ModelViewSet):
-    queryset = Address.objects.all().order_by('street')
+    queryset = Address.objects.all().order_by("street")
     serializer_class = AddressSerializer
+
 
 class InvitationViewSet(viewsets.ModelViewSet):
     queryset = Invitation.objects.all()
     serializer_class = InvitationSerializer
+
+
+class ServiceViewSet(viewsets.ModelViewSet):
+    queryset = Service.objects.all()
+    serializer_class = ServiceSerializer
