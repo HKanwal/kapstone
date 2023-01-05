f6bae7c77369617c7996adeb42ad93c3a9ededb4
Himanshu Aggarwal <aggarwal.h@outlook.com>
2023-01-05 16:48:33 -0500

    Register appointment related urls

--- src/django-app/shops/urls.py
+++ src/django-app/shops/urls.py
@@ -7,6 +7,8 @@ router.register(r'shops', views.ShopViewSet)
 router.register(r'addresses', views.AddressViewSet)
 router.register(r'invitations', views.InvitationViewSet)
 router.register(r'services', views.ServiceViewSet)
+router.register(r'appointment-slots', views.AppointmentSlotViewSet)
+router.register(r'appointments', views.AppointmentViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
