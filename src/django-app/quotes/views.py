a87ae4825b66533462ee4f254cc072043a0665ad
Himanshu Aggarwal <aggarwal.h@outlook.com>
2023-01-26 20:05:59 -0500

    Add permissions to quotes and quote requests

--- src/django-app/quotes/views.py
+++ src/django-app/quotes/views.py
@@ -1,14 +1,24 @@
 from django.shortcuts import render
 from rest_framework import viewsets
 
-from .serializers import QuoteRequestSerializer, QuoteSerializer
+from .serializers import (
+    QuoteRequestSerializer,
+    QuoteSerializer,
+    QuoteRequestWriteSerializer,
+)
 from .models import Quote, QuoteRequest
+from config.permissions import IsShopOwnerOrEmployee, IsOwner
+from .mixins import ReadWriteSerializerMixin
 
 
 class QuoteViewSet(viewsets.ModelViewSet):
-    queryset = Quote.objects.all().order_by('pk')
+    permission_classes = [IsShopOwnerOrEmployee]
+    queryset = Quote.objects.all().order_by("pk")
     serializer_class = QuoteSerializer
 
-class QuoteRequestViewSet(viewsets.ModelViewSet):
-    queryset = QuoteRequest.objects.all().order_by('pk')
-    serializer_class = QuoteRequestSerializer
+
+class QuoteRequestViewSet(ReadWriteSerializerMixin, viewsets.ModelViewSet):
+    permission_classes = [IsShopOwnerOrEmployee | IsOwner]
+    queryset = QuoteRequest.objects.all().order_by("pk")
+    read_serializer_class = QuoteRequestSerializer
+    write_serializer_class = QuoteRequestWriteSerializer
