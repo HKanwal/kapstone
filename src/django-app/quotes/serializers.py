09164ccc44cb03c74355b907b5e91e1978326958
tevisdoe <tevis.doe@gmail.com>
2023-01-29 17:43:46 -0500

    Add image upload to quote request create method.

--- src/django-app/quotes/serializers.py
+++ src/django-app/quotes/serializers.py
@@ -3,8 +3,9 @@ from rest_framework import serializers
 from .models import Quote, QuoteRequest
 from shops.models import Shop
 from shops.serializers import ShopOverviewSerializer
-from accounts.models import Customer
 from accounts.serializers import UserViewSerializer
+from misc.models import ImageQuote
+from misc.serializers import ImageQuoteSerializer
 
 
 class QuoteSerializer(serializers.ModelSerializer):
@@ -22,6 +23,10 @@ class QuoteSerializer(serializers.ModelSerializer):
 class QuoteRequestSerializer(serializers.ModelSerializer):
     shop = ShopOverviewSerializer()
     customer = UserViewSerializer(source="user")
+    images = ImageQuoteSerializer(many=True, read_only=True)
+    uploaded_images = serializers.ListField(
+        child = serializers.ImageField(max_length = 1000000, allow_empty_file = False, use_url = False),
+        write_only=True)
 
     class Meta:
         model = QuoteRequest
@@ -31,6 +36,17 @@ class QuoteRequestSerializer(serializers.ModelSerializer):
             "customer",
             "preferred_date",
             "preferred_time",
+            "preferred_phone_number",
+            "preferred_email",
             "description",
+            "images",
+            "uploaded_images"
         )
         read_only_fields = ["customer"]
+    
+    def create(self, validated_data):
+        uploaded_images = validated_data.pop("uploaded_images")
+        quote_request = QuoteRequest.objects.create(**validated_data)
+        for image in uploaded_images:
+            QR_image = ImageQuote.objects.create(quote_request=quote_request, image=image)
+        return quote_request
