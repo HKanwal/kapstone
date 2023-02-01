95bbef9f9fde70bbb41c87320f0d8aed04a7d27d
Himanshu Aggarwal <aggarwal.h@outlook.com>
2023-02-01 18:51:46 -0500

    Add work order access policy

--- src/django-app/shops/policies.py
+++ src/django-app/shops/policies.py
@@ -215,3 +215,57 @@ class AppointmentSlotAccessPolicy(AccessPolicy):
             appointment_slot = view.get_object()
             shop = appointment_slot.shop
         return shop.shop_owner == request.user or shop.has_employee(request.user.id)
+
+
+class WorkOrderAccessPolicy(AccessPolicy):
+    statements = [
+        {
+            "action": ["list", "retrieve"],
+            "principal": "authenticated",
+            "effect": "allow",
+            "condition_expression": [
+                "(user_type_is_shop_owner or user_type_is_employee)"
+            ],
+        },
+        {
+            "action": ["create", "destroy"],
+            "principal": "authenticated",
+            "effect": "allow",
+            "condition": ["is_shop_related"],
+        },
+        {
+            "action": ["partial_update"],
+            "principal": "authenticated",
+            "effect": "allow",
+            "condition": ["is_shop_related"],
+        },
+        {
+            "action": ["update"],
+            "principal": "*",
+            "effect": "deny",
+        },
+    ]
+
+    @classmethod
+    def scope_queryset(cls, request, qs):
+        if request.user.is_authenticated:
+            if request.user.type == "shop_owner":
+                return qs.filter(shop__shop_owner=request.user)
+            elif request.user.type == "employee":
+                employee_shop = EmployeeData.objects.get(user=request.user).shop
+                return qs.filter(shop=employee_shop)
+        return qs
+
+    def is_shop_related(self, request, view, action):
+        if action == "create":
+            shop = Shop.objects.get(id=request.data.get("shop"))
+        else:
+            work_order = view.get_object()
+            shop = work_order.shop
+        return shop.shop_owner == request.user or shop.has_employee(request.user.id)
+
+    def user_type_is_shop_owner(self, request, view, action):
+        return request.user.type == "shop_owner"
+
+    def user_type_is_employee(self, request, view, action):
+        return request.user.type == "employee"
