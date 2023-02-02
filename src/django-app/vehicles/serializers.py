2384a2741b92a957694c71473b200e59f560182f
Himanshu Aggarwal <aggarwal.h@outlook.com>
2023-02-02 18:39:42 -0500

    Add vehicle serializers

--- src/django-app/vehicles/serializers.py
+++ src/django-app/vehicles/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from .models import Part
+from .models import Part, Vehicle
 
 
 class PartSerializer(serializers.ModelSerializer):
@@ -8,3 +8,21 @@ class PartSerializer(serializers.ModelSerializer):
         model = Part
         fields = "__all__"
         read_only_fields = ("id",)
+
+
+class VehicleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Vehicle
+        fields = "__all__"
+        read_only_fields = ("id",)
+
+
+class VehicleWriteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Vehicle
+        fields = ("manufacturer", "model", "color", "year", "vin", "customer")
+        read_only_fields = ("id", "customer")
+
+    def create(self, validated_data):
+        validated_data["customer"] = self.context["request"].user
+        return Vehicle.objects.create(**validated_data)
