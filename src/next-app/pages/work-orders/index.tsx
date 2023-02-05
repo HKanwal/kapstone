43275ad93c95da0edcd773fab5aa33f349353337
Himanshu Aggarwal <aggarwal.h@outlook.com>
2023-02-04 23:20:49 -0500

    Update work order list URL

--- src/next-app/pages/work-orders/index.tsx
+++ src/next-app/pages/work-orders/index.tsx
@@ -4,6 +4,7 @@ import Header from '../../components/Header';
 import Link from 'next/link';
 import Moment from 'react-moment';
 import 'moment-timezone';
+import apiUrl from '../../constants/api-url';
 
 const WorkOrdersList: NextPage = ({ workOrders }: any) => {
   return (
@@ -48,7 +49,7 @@ export const getServerSideProps: GetServerSideProps<{}> = async () => {
   try {
     const token =
       'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjc2MjQ0OTgzLCJqdGkiOiIxZDg4MTJhYTg5ZmI0N2JjYjFlODU3ODU4NWZjMDNjMyIsInVzZXJfaWQiOjE0OH0.vZ8GjUqu9NUbGX4um7MSoCWI6OQVZrFDZQmJ6I57tlI';
-    const workOrders = await axios.get(`http://127.0.0.1:8000/shops/work-orders/`, {
+    const workOrders = await axios.get(`${apiUrl}/shops/work-orders/`, {
       headers: { Authorization: `JWT ${token}` },
     });
     return {
