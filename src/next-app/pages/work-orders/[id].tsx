d2335d74e203965e77104cd52478037000c4f385
Himanshu Aggarwal <aggarwal.h@outlook.com>
2023-02-11 01:18:43 -0500

    Add patch request; Add error handling

--- src/next-app/pages/work-orders/[id].tsx
+++ src/next-app/pages/work-orders/[id].tsx
@@ -6,6 +6,12 @@ import { useFormik, Field, FormikProvider } from 'formik';
 import FieldLabel from '../../components/FieldLabel';
 import { ChangeEventHandler, useState } from 'react';
 import Button from '../../components/Button';
+import { GrFormEdit, GrFormClose } from 'react-icons/gr';
+import { useRouter } from 'next/router';
+import * as yup from 'yup';
+
+const token =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjc2MjQ0OTgzLCJqdGkiOiIxZDg4MTJhYTg5ZmI0N2JjYjFlODU3ODU4NWZjMDNjMyIsInVzZXJfaWQiOjE0OH0.vZ8GjUqu9NUbGX4um7MSoCWI6OQVZrFDZQmJ6I57tlI';
 
 type CardTextFieldProps = {
   fieldValue: string | ReadonlyArray<string> | number | undefined;
@@ -14,6 +20,7 @@ type CardTextFieldProps = {
   fieldType: string;
   fieldRequired?: boolean | undefined;
   fieldDisabled?: boolean | undefined;
+  error?: any;
   onChange: ChangeEventHandler<HTMLInputElement> | undefined;
 };
 
@@ -24,6 +31,7 @@ type CardTextAreaProps = {
   fieldRows?: number | undefined;
   fieldRequired?: boolean | undefined;
   fieldDisabled?: boolean | undefined;
+  error?: any;
   onChange: ChangeEventHandler<HTMLTextAreaElement> | undefined;
 };
 
@@ -33,6 +41,7 @@ type CardSelectProps = {
   fieldRequired?: boolean | undefined;
   fieldDisabled?: boolean | undefined;
   options: ReadonlyArray<string> | undefined;
+  error?: any;
 };
 
 const CardTextField = (props: CardTextFieldProps) => {
@@ -47,6 +56,9 @@ const CardTextField = (props: CardTextFieldProps) => {
         onChange={props.onChange}
         disabled={props.fieldDisabled}
       />
+      <div className="flex flex-col">
+        <span className="error">{props.error}</span>
+      </div>
     </div>
   );
 };
@@ -63,6 +75,9 @@ const CardTextArea = (props: CardTextAreaProps) => {
         disabled={props.fieldDisabled}
         rows={props.fieldRows}
       />
+      <div className="flex flex-col">
+        <span className="error">{props.error}</span>
+      </div>
     </div>
   );
 };
@@ -71,15 +86,41 @@ const CardSelect = (props: CardSelectProps) => {
   return (
     <div className={`card-field flex flex-col row-gap-small`}>
       <FieldLabel label={props.fieldLabel} required={props.fieldRequired} />
-      <Field name={props.fieldName} as="select" className="input select" disabled={props.fieldDisabled}>
+      <Field
+        name={props.fieldName}
+        as="select"
+        className="input select"
+        disabled={props.fieldDisabled}
+      >
         {props.options}
       </Field>
+      <div className="flex flex-col">
+        <span className="error">{props.error}</span>
+      </div>
     </div>
   );
 };
 
 const WorkOrdersDetail: NextPage = ({ workOrder, employees }: any) => {
+  const router = useRouter();
+  const { id } = router.query;
   const [inEdit, setInEdit] = useState(false);
+  const [errors, setErrors] = useState([]);
+  const schema = yup.object().shape({
+    odometer_reading_before: yup.number().optional().positive().integer(),
+    odometer_reading_after: yup.number().optional().positive().integer(),
+    discount: yup.number().optional().positive(),
+    grand_total: yup.number().required().positive(),
+    notes: yup.string(),
+    appointment: yup.object().shape({
+      customer: yup.object().shape({
+        username: yup.string().required(),
+      }),
+    }),
+    employee: yup.object().shape({
+      id: yup.number().required().positive().integer(),
+    }),
+  });
   const form = useFormik({
     initialValues: {
       odometer_reading_before: workOrder.odometer_reading_before,
@@ -96,16 +137,51 @@ const WorkOrdersDetail: NextPage = ({ workOrder, employees }: any) => {
         id: workOrder.employee.id,
       },
     },
-    onSubmit: (values) => {
-      alert(JSON.stringify(values, null, 2));
+    validationSchema: schema,
+    onSubmit: async (values) => {
+      const valuesToSend = {
+        odometer_reading_before: values.odometer_reading_before,
+        odometer_reading_after: values.odometer_reading_after,
+        discount: values.discount,
+        grand_total: values.grand_total,
+        notes: values.notes,
+        employee: values.employee.id,
+      };
+
+      try {
+        const res = await axios.patch(`${apiUrl}/shops/work-orders/${id}/`, valuesToSend, {
+          headers: { Authorization: `JWT ${token}` },
+        });
+        if (res.status === 200) {
+          router.reload();
+        }
+      } catch (error: any) {
+        setErrors(error.response.data.errors);
+        scrollTo(0, 0);
+      }
     },
   });
 
   return (
     <div className="container">
-      <Header title={`Work Order: #${workOrder.id}`} />
+      <Header
+        title={`Work Order: #${workOrder.id}`}
+        rightIcon={inEdit ? GrFormClose : GrFormEdit}
+        onRightIconClick={() => setInEdit(!inEdit)}
+      />
       <div className="wrapper">
         <div className="flex flex-row row-gap-large">
+          {errors.length > 0 && (
+            <div className="flex flex-col row-gap-small">
+              {errors.map((error: any, index) => {
+                return (
+                  <span className="error" key={`error_${index}`}>
+                    {error.detail}
+                  </span>
+                );
+              })}
+            </div>
+          )}
           <FormikProvider value={form}>
             <form onSubmit={form.handleSubmit}>
               <h2 className="form-header">Work Order Information</h2>
@@ -118,6 +194,7 @@ const WorkOrdersDetail: NextPage = ({ workOrder, employees }: any) => {
                   fieldRequired
                   fieldDisabled={true}
                   onChange={form.handleChange}
+                  error={form.errors.appointment?.customer?.username}
                 />
                 <CardSelect
                   fieldName="employee.id"
@@ -131,32 +208,7 @@ const WorkOrdersDetail: NextPage = ({ workOrder, employees }: any) => {
                   })}
                   fieldRequired
                   fieldDisabled={!inEdit}
-                />
-                <CardTextField
-                  fieldValue={form.values.odometer_reading_before}
-                  fieldName="odometer_reading_before"
-                  fieldLabel="Odometer Reading (Before Service)"
-                  fieldType="number"
-                  fieldRequired
-                  fieldDisabled={!inEdit}
-                  onChange={form.handleChange}
-                />
-                <CardTextField
-                  fieldValue={form.values.odometer_reading_after}
-                  fieldName="odometer_reading_after"
-                  fieldLabel="Odometer Reading (After Service)"
-                  fieldType="number"
-                  fieldRequired
-                  fieldDisabled={!inEdit}
-                  onChange={form.handleChange}
-                />
-                <CardTextField
-                  fieldValue={form.values.discount}
-                  fieldName="discount"
-                  fieldLabel="Discount ($)"
-                  fieldType="number"
-                  fieldDisabled={!inEdit}
-                  onChange={form.handleChange}
+                  error={form.errors.employee?.id}
                 />
                 <CardTextField
                   fieldValue={form.values.grand_total}
@@ -165,6 +217,35 @@ const WorkOrdersDetail: NextPage = ({ workOrder, employees }: any) => {
                   fieldType="number"
                   fieldDisabled={!inEdit}
                   onChange={form.handleChange}
+                  error={form.errors.grand_total}
+                  fieldRequired
+                />
+                <CardTextField
+                  fieldValue={form.values.discount}
+                  fieldName="discount"
+                  fieldLabel="Discount ($)"
+                  fieldType="number"
+                  fieldDisabled={!inEdit}
+                  onChange={form.handleChange}
+                  error={form.errors.discount}
+                />
+                <CardTextField
+                  fieldValue={form.values.odometer_reading_before}
+                  fieldName="odometer_reading_before"
+                  fieldLabel="Odometer Reading (Before Service)"
+                  fieldType="number"
+                  fieldDisabled={!inEdit}
+                  onChange={form.handleChange}
+                  error={form.errors.odometer_reading_before}
+                />
+                <CardTextField
+                  fieldValue={form.values.odometer_reading_after}
+                  fieldName="odometer_reading_after"
+                  fieldLabel="Odometer Reading (After Service)"
+                  fieldType="number"
+                  fieldDisabled={!inEdit}
+                  onChange={form.handleChange}
+                  error={form.errors.odometer_reading_after}
                 />
                 <CardTextArea
                   fieldValue={form.values.notes}
@@ -172,8 +253,9 @@ const WorkOrdersDetail: NextPage = ({ workOrder, employees }: any) => {
                   fieldLabel="Notes"
                   fieldDisabled={!inEdit}
                   onChange={form.handleChange}
+                  error={form.errors.notes}
                 />
-                <Button type="submit" title="Submit" width={'100%'}></Button>
+                {inEdit && <Button type="submit" title="Save" width={'100%'}></Button>}
               </div>
             </form>
           </FormikProvider>
@@ -186,8 +268,6 @@ const WorkOrdersDetail: NextPage = ({ workOrder, employees }: any) => {
 export const getServerSideProps: GetServerSideProps<{}> = async (context) => {
   const { id } = context.query;
   try {
-    const token =
-      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjc2MjQ0OTgzLCJqdGkiOiIxZDg4MTJhYTg5ZmI0N2JjYjFlODU3ODU4NWZjMDNjMyIsInVzZXJfaWQiOjE0OH0.vZ8GjUqu9NUbGX4um7MSoCWI6OQVZrFDZQmJ6I57tlI';
     const workOrder = await axios.get(`${apiUrl}/shops/work-orders/${id}`, {
       headers: { Authorization: `JWT ${token}` },
     });
