55159bf09e318cdb590349ed8c5195ef1fea22a6
Himanshu Aggarwal <aggarwal.h@outlook.com>
2023-02-13 16:18:43 -0500

    Fix css not loading in production

--- src/next-app/pages/_app.tsx
+++ src/next-app/pages/_app.tsx
@@ -54,10 +54,6 @@ function MyApp({ Component, pageProps }: AppProps) {
             <link rel="manifest" href="/manifest.json" />
             <link rel="mask-icon" href="/icons/safari-pinned-tab.svg" color="#5bbad5" />
             <link rel="shortcut icon" href="/favicon.ico" />
-            <link
-              rel="stylesheet"
-              href="https://fonts.googleapis.com/css?family=Roboto:300,400,500"
-            />
             <meta name="twitter:card" content="summary" />
             <meta name="twitter:url" content="https://yourdomain.com" />
             <meta name="twitter:title" content="PWA App" />
