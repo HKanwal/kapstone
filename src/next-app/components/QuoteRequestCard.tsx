a0977306caad759900a64c01f5492694b1491b97
Rachel Johnson <rachelj1098@yahoo.ca>
2023-02-13 17:33:49 -0500

    fixed quote request card

--- src/next-app/components/QuoteRequestCard.tsx
+++ src/next-app/components/QuoteRequestCard.tsx
@@ -5,15 +5,18 @@ import React from 'react';
 type QuoteRequestProps = {
   id: number;
   description: string;
-  dateCreated: string;
+  dateCreated?: string;
   path?: string;
 };
 
 const QuoteRequestCard = (props: QuoteRequestProps) => {
   let descriptionLabel = props.description;
-  const tempDate = new Date(props.dateCreated);
-  const tempDateString = tempDate.toDateString();
-  const dateLabel = `Date: ${tempDateString}`;
+  let dateLabel = '';
+  if (props.dateCreated) {
+    const tempDate = new Date(props.dateCreated);
+    const tempDateString = tempDate.toDateString();
+    dateLabel = `Date: ${tempDateString}`;
+  }
   const router = useRouter();
   const url = '/quote-request-details';
 
