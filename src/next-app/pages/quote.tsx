093004418e34aa8c0b588910c295e31db97a52da
Gilbert Cherrie <g.cherrie@outlook.com>
2023-02-13 13:55:38 -0500

    Add code to load page without id

--- src/next-app/pages/quote.tsx
+++ src/next-app/pages/quote.tsx
@@ -6,7 +6,8 @@ import { useRouter } from 'next/router';
 import apiUrl from '../constants/api-url';
 import TextField from '../components/TextField';
 import Button from '../components/Button';
-import { AuthContext } from '../utils/api';
+import { AuthContext, accountTypes } from '../utils/api';
+import Cookies from 'js-cookie';
 
 type QuoteProps = {
   id: number;
@@ -22,8 +23,11 @@ type QuoteProps = {
 };
 
 const Quote: NextPage = (props) => {
+  const [quoteData, setQuoteData] = useState({} as QuoteProps);
+  const [authData, setAuthData] = useState(useContext(AuthContext));
+
   const router = useRouter();
-  console.log(router.query);
+  const { id } = router.query;
 
   if (authData.access !== '') {
   } else if (Cookies.get('access') && Cookies.get('access') !== '') {
@@ -36,8 +40,6 @@ const Quote: NextPage = (props) => {
     )
   }
 
-
-  const [quoteData, setQuoteData] = useState({} as QuoteProps);
 
   const sampleData = {
     id: 0,
@@ -71,104 +73,117 @@ const Quote: NextPage = (props) => {
     window.open(`tel:${sampleData.shopNumber}`);
   }
 
-  return (
-    <div className={styles.container}>
-      <Header title={`Quote - ${quoteData.description}`} burgerMenu={[{
-        option: "Call Shop", onClick() {
-          handleCallClick();
-        },
-      }]} />
-      <div className={styles["quote-container"]}>
-        <div className={styles["status-container"]}>
-          <label>
-            {`Status: ${quoteData.status}`}
-          </label>
-        </div>
-        <div className={styles["cost-container"]}>
-          <label>
-            {`Cost: $${quoteData.cost}`}
-          </label>
-        </div>
-        <div className={styles["date-container"]}>
-          <label>
-            {`Date: ${quoteData.date}`}
-          </label>
-        </div>
-        <div className={styles['field-container']}>
-          <TextField
-            name="Accepted By"
-            placeholder={quoteData.acceptedBy}
-            disabled={true}
-          />
-        </div>
-        {quoteData.partCost || quoteData.labourCost ? (
-          < div className={styles.section}>
-            <div className={styles['section-header']}>
-              <label>
-                Cost Breakdown:
-              </label>
+  if (id) {
+    return (
+      <div className={styles.container}>
+        <Header title={`Quote - ${quoteData.description}`} burgerMenu={[{
+          option: "Call Shop", onClick() {
+            handleCallClick();
+          },
+        }]} />
+        <div className={styles["quote-container"]}>
+          <div className={styles["status-container"]}>
+            <label>
+              {`Status: ${quoteData.status}`}
+            </label>
+          </div>
+          <div className={styles["cost-container"]}>
+            <label>
+              {`Cost: $${quoteData.cost}`}
+            </label>
+          </div>
+          <div className={styles["date-container"]}>
+            <label>
+              {`Date: ${quoteData.date}`}
+            </label>
+          </div>
+          <div className={styles['field-container']}>
+            <TextField
+              name="Accepted By"
+              placeholder={quoteData.acceptedBy}
+              disabled={true}
+            />
+          </div>
+          {quoteData.partCost || quoteData.labourCost ? (
+            < div className={styles.section}>
+              <div className={styles['section-header']}>
+                <label>
+                  Cost Breakdown:
+                </label>
+              </div>
+              {quoteData.partCost ? (
+                <div className={styles['field-container']}>
+                  <TextField
+                    name="Parts Cost:"
+                    placeholder={`$${quoteData.partCost}`}
+                    disabled={true}
+                  />
+                </div>) : null}
+              {quoteData.labourCost ? (
+                <div className={styles['field-container']}>
+                  <TextField
+                    name="Labour Cost:"
+                    placeholder={`$${quoteData.labourCost}`}
+                    disabled={true}
+                  />
+                </div>) : null}
+            </div>)
+            : null}
+          <div className={styles['field-container']}>
+            <TextField
+              name="Estimated Time:"
+              placeholder={quoteData.estimatedTime}
+              disabled={true}
+            />
+          </div>
+          <div className={styles['field-container']}>
+            <TextField
+              name="Quote Expires On:"
+              placeholder={quoteData.dueDate}
+              disabled={true}
+            />
+          </div>
+          <div className={styles['buttons-container']}>
+            <div className={styles['reject-button']}>
+              <Button
+                title="Reject Quote"
+                width="80%"
+                backgroundColor="red"
+                onClick={() => {
+                  console.log(
+                    'TODO: API call for rejecting quote'
+                  );
+                }}
+              />
             </div>
-            {quoteData.partCost ? (
-              <div className={styles['field-container']}>
-                <TextField
-                  name="Parts Cost:"
-                  placeholder={`$${quoteData.partCost}`}
-                  disabled={true}
-                />
-              </div>) : null}
-            {quoteData.labourCost ? (
-              <div className={styles['field-container']}>
-                <TextField
-                  name="Labour Cost:"
-                  placeholder={`$${quoteData.labourCost}`}
-                  disabled={true}
-                />
-              </div>) : null}
-          </div>)
-          : null}
-        <div className={styles['field-container']}>
-          <TextField
-            name="Estimated Time:"
-            placeholder={quoteData.estimatedTime}
-            disabled={true}
-          />
-        </div>
-        <div className={styles['field-container']}>
-          <TextField
-            name="Quote Expires On:"
-            placeholder={quoteData.dueDate}
-            disabled={true}
-          />
-        </div>
-        <div className={styles['buttons-container']}>
-          <div className={styles['reject-button']}>
-            <Button
-              title="Reject Quote"
-              width="80%"
-              backgroundColor="red"
-              onClick={() => {
-                console.log(
-                  'TODO: API call for rejecting quote'
-                );
-              }}
-            />
+            <div className={styles['accept-button']}>
+              <Button
+                title="Accept Quote"
+                margin="0 0 0 9vw"
+                width="80%"
+                onClick={() => {
+                  console.log(
+                    'TODO: API call for accepting quote'
+                  );
+                }}
+              />
+            </div>
           </div>
-          <div className={styles['accept-button']}>
-            <Button
-              title="Accept Quote"
-              margin="0 0 0 9vw"
-              width="80%"
-              onClick={() => {
-                console.log(
-                  'TODO: API call for accepting quote'
-                );
-              }}
-            />
-          </div>
+        </div>
+      </div >
+    )
+  } else {
+    return (
+      <div className={styles.container}>
+        <Header title={`Quote`} />
+        <div className={styles['error-page-container']}>
+          <label>
+            No Quote Request Selected
+          </label>
         </div>
       </div>
-    </div >
-  )
+    )
+  }
 };
 
 export default Quote;
