8333b5557f5b26ef4c74724d975f3ea9864390ea
Harkeerat Kanwal <hsk532991@gmail.com>
2023-02-13 20:47:22 -0500

    Finished calendar look; need to add on-click logic to time-slots

--- src/next-app/pages/book-appointment.tsx
+++ src/next-app/pages/book-appointment.tsx
@@ -5,10 +5,14 @@ import Header from '../components/Header';
 import { BsChevronLeft, BsChevronRight } from 'react-icons/bs'; // TODO: Change icons to left and right carets
 import IconButton from '../components/IconButton';
 import DatePicker from '../components/DatePicker';
-import { useState } from 'react';
+import { useMemo, useState } from 'react';
 
-function timeToString(t: number): string {
-  return (t - t % 1) + ':00';
+function createEmptyArray(n: number): undefined[] {
+  let a = [];
+  for (let i = 0; i < n; i++) {
+    a.push(undefined);
+  }
+  return a;
 }
 
 const BookAppointmentPage: NextPage = () => {
@@ -16,8 +20,13 @@ const BookAppointmentPage: NextPage = () => {
   const [date, setDate] = useState(new Date());
 
   // all times are represented as a number between 0 and 24
-  const [startTime, setStartTime] = useState(7);
-  const [endTime, setEndTime] = useState(15);
+  // start time can only be x.00 (xx:00), x.25 (xx:15), x.50 (xx:30), or x.75 (xx:45)
+  const [startTime, setStartTime] = useState(6.5);
+  const [endTime, setEndTime] = useState(11);
+
+  // # of sub-slots before first xx:00 mark
+  // minimum of 1 for aesthetic purposes
+  const preSlots = (startTime % 1) / 0.25 || 1;
 
   const handleDateChange = (newDates: Date[]) => {
     setDate(newDates[0]);
@@ -51,9 +60,32 @@ const BookAppointmentPage: NextPage = () => {
         </div>
         <div className={styles.calendar}>
           <div className={styles['time-row']}>
-            <span className={styles.time}>7:00</span>
-            <div className={styles.slot}></div>
+            <div className={styles['time-container']}>
+              <div className={styles['time-inner-container']}>
+                <span className={styles.time}>{Math.floor(startTime)}:00</span>
+              </div>
+            </div>
+            <div className={styles.slot}>
+              {createEmptyArray(preSlots).map((u, i) => (
+                <div className={styles['sub-slot']} key={i}></div>
+              ))}
+            </div>
           </div>
+          {createEmptyArray(Math.ceil(endTime) - Math.floor(startTime)).map((u, i) => (
+            <div className={styles['time-row']} key={i}>
+              <div className={styles['time-container']}>
+                <div className={styles['time-inner-container']}>
+                  <span className={styles.time}>{Math.floor(startTime) + i + 1}:00</span>
+                </div>
+              </div>
+              <div className={styles.slot}>
+                <div className={styles['sub-slot']}></div>
+                <div className={styles['sub-slot']}></div>
+                <div className={styles['sub-slot']}></div>
+                <div className={styles['sub-slot']}></div>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
     </div>
