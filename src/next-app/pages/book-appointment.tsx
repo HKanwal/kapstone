edf5c08a584916c3a6cedd503db68ef42e8feca1
HKanwal <hsk532991@gmail.com>
2023-02-13 19:20:53 -0500

    Working on calendar

--- src/next-app/pages/book-appointment.tsx
+++ src/next-app/pages/book-appointment.tsx
@@ -7,9 +7,17 @@ import IconButton from '../components/IconButton';
 import DatePicker from '../components/DatePicker';
 import { useState } from 'react';
 
+function timeToString(t: number): string {
+  return (t - t % 1) + ':00';
+}
+
 const BookAppointmentPage: NextPage = () => {
   const router = useRouter();
   const [date, setDate] = useState(new Date());
+
+  // all times are represented as a number between 0 and 24
+  const [startTime, setStartTime] = useState(7);
+  const [endTime, setEndTime] = useState(15);
 
   const handleDateChange = (newDates: Date[]) => {
     setDate(newDates[0]);
@@ -36,10 +44,16 @@ const BookAppointmentPage: NextPage = () => {
       <Header title="Book Appointment" />
 
       <div className={styles.content}>
-        <div className={styles.a}>
+        <div className={styles['date-selection']}>
           <IconButton icon={BsChevronLeft} onClick={decrementDate} />
           <DatePicker value={[date]} onChange={handleDateChange} single />
           <IconButton icon={BsChevronRight} onClick={incrementDate} />
+        </div>
+        <div className={styles.calendar}>
+          <div className={styles['time-row']}>
+            <span className={styles.time}>7:00</span>
+            <div className={styles.slot}></div>
+          </div>
         </div>
       </div>
     </div>
