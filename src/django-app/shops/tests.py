691f82f133a1e03ad2c7e2cc56db5101e0c81228
tevisdoe <tevis.doe@gmail.com>
2023-02-21 16:15:50 -0500

    Add basic shop test.

--- src/django-app/shops/tests.py
+++ src/django-app/shops/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from .models import Shop, Address
+from accounts.models import User, ShopOwner
 
-# Create your tests here.
+class BasicShopTestCase(TestCase):
+    def setUp(self):
+        Address.objects.create(postal_code="M4E2T2", street="104 Test Street", city="Toronto", country="Canada", province="ON")
+        ShopOwner.objects.create_user(username="testuser", password="qwerty123", email="testemail@test.com")
+
+    def test_shop_creation(self):
+        
+        shop_owner = User.objects.get(username="testuser")
+        address = Address.objects.get(street="104 Test Street")
+        
+        shop = Shop.objects.create(shop_owner=shop_owner, shop_email="testemail@test.com", address=address)
+
+        self.assertEqual(shop, Shop.objects.get(shop_owner=shop_owner))
