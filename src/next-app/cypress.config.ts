ca15a77b7e9acaeb6d12c5fe079745f611b5ad24
Himanshu Aggarwal <aggarwal.h@outlook.com>
2023-03-04 13:10:15 -0500

    Add API key for email testing

--- src/next-app/cypress.config.ts
+++ src/next-app/cypress.config.ts
@@ -5,8 +5,12 @@ export default defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    baseUrl: 'http://localhost:3000',
+    baseUrl: 'https://kapstone-ten.vercel.app',
   },
   viewportWidth: 414,
   viewportHeight: 896,
+  env: {
+    TESTMAIL_APIKEY: 'c3a5621d-f55b-4934-b6ae-47bbc0654055',
+    TESTMAIL_NAMESPACE: 'dl4z5',
+  },
 });
