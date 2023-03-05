565695148f7ee28e45b7dda41a7ad10d9fb46bb9
Himanshu Aggarwal <aggarwal.h@outlook.com>
2023-03-05 16:53:57 -0500

    Add cypress env variables

--- src/next-app/cypress.config.ts
+++ src/next-app/cypress.config.ts
@@ -12,5 +12,12 @@ export default defineConfig({
   env: {
     TESTMAIL_APIKEY: 'c3a5621d-f55b-4934-b6ae-47bbc0654055',
     TESTMAIL_NAMESPACE: 'dl4z5',
+    TEST_SHOP_OWNER_USERNAME: 'sam',
+    TEST_SHOP_OWNER_PASSWORD: 'pass101word',
+    TEST_EMPLOYEE_USERNAME: 'ethan',
+    TEST_EMPLOYEE_PASSWORD: 'pass101word',
+    TEST_CUSTOMER_USERNAME: 'chris',
+    TEST_CUSTOMER_PASSWORD: 'pass101word',
+    TEST_WORK_ORDER_ID: 1,
   },
 });
