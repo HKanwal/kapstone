46296cac7334ee22f24987aaa8a56f7a7fb523fe
Harkeerat Kanwal <hsk532991@gmail.com>
2023-03-04 23:58:32 -0500

    Add style test for button

--- src/next-app/__tests__/Button.test.tsx
+++ src/next-app/__tests__/Button.test.tsx
@@ -7,4 +7,10 @@ describe('Button', () => {
     render(<Button title="Click Me!" />);
     expect(screen.getByText('Click Me!')).toBeInTheDocument();
   });
+
+  it('renders with correct width', () => {
+    const width = '50%';
+    render(<Button title="Click Me!" width={width} />);
+    expect(screen.getByText('Click Me!')).toHaveStyle(`width: ${width}`);
+  });
 });
