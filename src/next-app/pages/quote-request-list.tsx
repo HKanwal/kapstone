85bcb4b6f537160819e7db9e3f8af66d63a2e4ac
Rachel Johnson <rachelj1098@yahoo.ca>
2023-03-10 01:27:15 -0500

    changed access to quote requests for  non-users

--- src/next-app/pages/quote-request-list.tsx
+++ src/next-app/pages/quote-request-list.tsx
@@ -9,11 +9,14 @@ import { IoIosAdd } from 'react-icons/io';
 import { AuthContext, accountTypes } from '../utils/api';
 import Cookies from 'js-cookie';
 import { GrAddCircle } from 'react-icons/gr';
+import Modal from '../components/Modal';
+import Button from '../components/Button';
 
 const QuoteRequestListPage: NextPage = () => {
   const router = useRouter();
   const [quoteRequestCards, setQuoteRequestCards] = useState([] as JSX.Element[]);
   const [authData, setAuthData] = useState(useContext(AuthContext));
+  const [modalVisible, setModalVisisble] = useState<boolean>(false);
 
   if (authData.access !== '') {
   } else if (Cookies.get('access') && Cookies.get('access') !== '') {
@@ -23,56 +26,85 @@ const QuoteRequestListPage: NextPage = () => {
       user_type: Cookies.get('user_type') as accountTypes,
     });
   }
-
+  console.log(Cookies.get('access'));
   useEffect(() => {
-    const cards: JSX.Element[] = [];
-    fetch(`${apiUrl}/quotes/quote-requests/`, {
-      method: 'GET',
-      headers: {
-        Authorization: `JWT ${authData.access}`,
-        'Content-Type': 'application/json; charset=UTF-8',
-      },
-    }).then((data) => {
-      console.log(data);
-      data.json().then((data) => {
+    if (Cookies.get('access') !== undefined) {
+      const cards: JSX.Element[] = [];
+      fetch(`${apiUrl}/quotes/quote-requests/`, {
+        method: 'GET',
+        headers: {
+          Authorization: `JWT ${authData.access}`,
+          'Content-Type': 'application/json; charset=UTF-8',
+        },
+      }).then((data) => {
         console.log(data);
-        data.forEach((quoteRequest: any) => {
-          //console.log(quoteRequest);
-          // cards.push(<QuoteRequestCard id={quoteRequest.id} description={quoteRequest.description} dateCreated={quoteRequest.dateCreated} />)
-          cards.push(
-            <QuoteRequestCard
-              key={quoteRequest.id}
-              id={quoteRequest.id}
-              description={quoteRequest.description}
-              dateCreated={quoteRequest.created_at}
-            />
-          );
+        data.json().then((data) => {
+          console.log(data);
+          data.forEach((quoteRequest: any) => {
+            //console.log(quoteRequest);
+            // cards.push(<QuoteRequestCard id={quoteRequest.id} description={quoteRequest.description} dateCreated={quoteRequest.dateCreated} />)
+            cards.push(
+              <QuoteRequestCard
+                key={quoteRequest.id}
+                id={quoteRequest.id}
+                description={quoteRequest.description}
+                dateCreated={quoteRequest.created_at}
+              />
+            );
+          });
+          setQuoteRequestCards(cards);
         });
-        setQuoteRequestCards(cards);
       });
-    });
 
-    // TODO: Replace sample data with API data and place in above .then handler
-    // sampleData.sort((a, b) => {
-    //   let keyA = new Date(a.dateCreated);
-    //   let keyB = new Date(b.dateCreated);
-    //   if (keyA < keyB) return 1;
-    //   if (keyA > keyB) return -1;
-    //   return 0;
-    // });
-    // sampleData.forEach((quoteRequest) => {
-    //   cards.push(<QuoteRequestCard id={quoteRequest.id} description={quoteRequest.description} dateCreated={quoteRequest.dateCreated} />)
-    // });
+      // TODO: Replace sample data with API data and place in above .then handler
+      // sampleData.sort((a, b) => {
+      //   let keyA = new Date(a.dateCreated);
+      //   let keyB = new Date(b.dateCreated);
+      //   if (keyA < keyB) return 1;
+      //   if (keyA > keyB) return -1;
+      //   return 0;
+      // });
+      // sampleData.forEach((quoteRequest) => {
+      //   cards.push(<QuoteRequestCard id={quoteRequest.id} description={quoteRequest.description} dateCreated={quoteRequest.dateCreated} />)
+      // });
+    }
   }, []);
 
   const handleAddClick = () => {
     router.push('/quote-request');
   };
 
+  const showModal = () => {
+    setModalVisisble(true);
+  };
+
+  const hideModal = () => {
+    setModalVisisble(false);
+  };
+
   return (
     <div className={styles.container}>
-      <Header title="Quote Requests" rightIcon={GrAddCircle} onRightIconClick={handleAddClick} />
+      <Header
+        title="Quote Requests"
+        rightIcon={GrAddCircle}
+        onRightIconClick={Cookies.get('access') ? handleAddClick : showModal}
+      />
       <div className={styles['card-container']}>{quoteRequestCards}</div>
+      <Modal visible={modalVisible} onClose={hideModal}>
+        <div className={styles['modal-content']}>
+          <div className={styles['modal-title-container']}>
+            <span className={styles['modal-title']}>
+              Login or Register to Access Quote Request Creation
+            </span>
+          </div>
+          <div className={styles['modal-submit']}>
+            <Button title="Log In" onClick={() => router.push('/login')} width="100%" />
+          </div>
+          <div className={styles['modal-submit']}>
+            <Button title="Register" onClick={() => router.push('/create-account')} width="100%" />
+          </div>
+        </div>
+      </Modal>
     </div>
   );
 };
