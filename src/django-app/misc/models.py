f3a365e199c99d02a4eb5e8bd605205b276e1fc8
Himanshu Aggarwal <aggarwal.h@outlook.com>
2023-03-19 22:18:27 -0400

    Add batch id to ImageQuote model

--- src/django-app/misc/models.py
+++ src/django-app/misc/models.py
@@ -1,10 +1,25 @@
 from django.db import models
 from django.utils.translation import gettext as _
 from quotes.models import QuoteRequest
+from django.db.models import Q
+
 
 def upload_to(instance, filename):
-    return 'quote_images/{filename}'.format(filename=filename)
+    return "quote_images/{filename}".format(filename=filename)
+
 
 class ImageQuote(models.Model):
     photo = models.ImageField(_("Image"), upload_to=upload_to)
     quote_request = models.ForeignKey(QuoteRequest, on_delete=models.CASCADE, null=True)
+    quote_request_batch_id = models.UUIDField(default=None, null=True)
+
+    class Meta:
+        constraints = [
+            models.CheckConstraint(
+                check=Q(
+                    quote_request_batch_id__isnull=True, quote_request__isnull=False
+                )
+                | Q(quote_request_batch_id__isnull=False, quote_request__isnull=True),
+                name="Either provide a quote request or a quote request batch id",
+            )
+        ]
